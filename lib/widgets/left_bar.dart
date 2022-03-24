f1350a402b63479c73209519add9024ad67538f9
Neel Kothari <neelkothariak@gmail.com>
2022-03-25 00:24:19 +0530

    first commit

---
+++ lib/widgets/left_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_app_flutter/constants/app_constants.dart';
+
+class LeftBar extends StatelessWidget {
+  final double barWidth;
+
+  const LeftBar({Key? key, required this.barWidth}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            
+            ),
+            color: accentHexColor
+
+          ),
+        )
+        
+      ],
+    );
+  }
+}
